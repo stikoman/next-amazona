7c61ab77a3603bb2b4764addcd5f9ed06f6955d2
Steven Kwasiba <stikowebdevelopment@gmail.com>
2022-05-06 22:18:46 -0300

    herstel

--- pages/cart.js
+++ pages/cart.js
@@ -4,23 +4,7 @@ import Layout from '../components/Layout';
 import { Store } from '../utils/Store';
 import NextLink from 'next/link';
 import Image from 'next/image';
-import {
-  Grid,
-  TableContainer,
-  Table,
-  Typography,
-  TableHead,
-  TableBody,
-  TableRow,
-  TableCell,
-  Link,
-  Select,
-  MenuItem,
-  Button,
-  Card,
-  List,
-  ListItem,
-} from '@material-ui/core';
+import {} from '@material-ui/core';
 import axios from 'axios';
 import { useRouter } from 'next/router';
 
